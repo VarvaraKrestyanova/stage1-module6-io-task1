2fd11c771143eeedbf94fdbc56e0482468a58032
VarvaraKrestyanova <krestyanovav@gmail.com>
2022-12-03 23:40:59 +0800

    task in progress

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        String profileData = "";
+        try(java.io.FileReader reader = new java.io.FileReader(file)) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                profileData += (char) character;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String name; Integer age; String email; Long phone;
+
+
         return new Profile();
+    }
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        File file = new File("src/main/resources/Profile.txt");
+        fileReader.getDataFromFile(file);
     }
 }
