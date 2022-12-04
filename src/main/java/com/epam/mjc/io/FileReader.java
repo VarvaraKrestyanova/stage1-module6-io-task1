6bdadb767a3a03551ee994e579f4698381d13e25
VarvaraKrestyanova <krestyanovav@gmail.com>
2022-12-04 14:45:16 +0800

    updated code to read in better way using buffer

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,5 +1,6 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,25 +9,27 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        String profileData = "";
-        try(java.io.FileReader reader = new java.io.FileReader(file)) {
-            int character;
-            while ((character = reader.read()) != -1) {
-                profileData += (char) character;
+
+        String name = null; Integer age = null; String email = null; Long phone = null;
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String[] arrLineData = new String[2];
+            String line;
+            while ((line = reader.readLine()) != null) {
+                arrLineData = line.split(": ");
+                switch (arrLineData[0]) {
+                    case "Name" -> name = arrLineData[1];
+                    case "Age" -> age = Integer.valueOf(arrLineData[1]);
+                    case "Email" -> email = arrLineData[1];
+                    case "Phone" -> phone = Long.valueOf(arrLineData[1]);
+                }
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("The provided file is not found: " + file.getAbsolutePath(), e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        String name; Integer age; String email; Long phone;
-
-
-        return new Profile();
+        return new Profile(name, age, email, phone);
     }
 
-    public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        File file = new File("src/main/resources/Profile.txt");
-        fileReader.getDataFromFile(file);
-    }
 }
